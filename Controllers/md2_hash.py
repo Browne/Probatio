c746b5ee859fcb085c9a6a8d70bb9265e694c76f
Eliot Brown <eliotjbrown@gmail.com>
2012-12-27 20:06:50 +0000

    The md2 hashing algorithm

---
+++ Controllers/md2_hash.py
@@ -0,0 +1,85 @@
+#!usr/bin/env python
+
+""" Calculate the md2 hash of a word """
+
+__author__ = "Eliot Brown"
+__copyright__ = "Copyright 2010 - 2013, Eliot Brown"
+
+__license__ = "MPLv2"
+__version__ = "1.11"
+__maintainer__ = "Eliot Brown"
+__email__ = "eliotjbrown@gmail.com"
+__status__ = "Production"
+
+from binascii import hexlify
+
+def md2(M):
+    ''' Calculates the md2 hash of M.
+    See http://www.networksorcery.com/enp/rfc/rfc1319.txt
+    and http://www.rfc-editor.org/errata_search.php?rfc=1319
+ 
+    RFC1319 test suite:
+    >>> md2('')
+    '8350e5a3e24c153df2275c9f80692773'
+    >>> md2('a')
+    '32ec01ec4a6dac72c0ab96fb34c0b5d1'
+    >>> md2('abc')
+    'da853b0d3f88d99b30283a69e6ded6bb'
+    >>> md2('message digest')
+    'ab4f496bfb2a530b219ff33031fe06b0'
+    >>> md2('abcdefghijklmnopqrstuvwxyz')
+    '4e8ddff3650292ab5a4108c3aa47940b'
+    >>> md2('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+    'da33def2a42df13975352846c30338cd'
+    >>> md2('1234567890123456789012345678901234567890123456789012345678901234'
+    ...     + '5678901234567890')
+    'd5976f79d83d3a0dc9806c3c66f3efd8'
+    '''
+    M = list(M)
+    # Step 1. Append Padding Bytes
+    i = 1
+    while (len(M) + i) % 16 != 0:
+        i += 1
+    M += list(chr(i) * i)
+    # Step 2. Append Checksum
+    C = list(chr(0) * 16)
+    L = 0
+    S = map(chr, [41, 46, 67, 201, 162, 216, 124, 1, 61, 54, 84, 161, 236,
+        240, 6, 19, 98, 167, 5, 243, 192, 199, 115, 140, 152, 147, 43, 217,
+        188, 76, 130, 202, 30, 155, 87, 60, 253, 212, 224, 22, 103, 66, 111,
+        24, 138, 23, 229, 18, 190, 78, 196, 214, 218, 158, 222, 73, 160, 251,
+        245, 142, 187, 47, 238, 122, 169, 104, 121, 145, 21, 178, 7, 63, 148,
+        194, 16, 137, 11, 34, 95, 33, 128, 127, 93, 154, 90, 144, 50, 39, 53,
+        62, 204, 231, 191, 247, 151, 3, 255, 25, 48, 179, 72, 165, 181, 209,
+        215, 94, 146, 42, 172, 86, 170, 198, 79, 184, 56, 210, 150, 164, 125,
+        182, 118, 252, 107, 226, 156, 116, 4, 241, 69, 157, 112, 89, 100, 113,
+        135, 32, 134, 91, 207, 101, 230, 45, 168, 2, 27, 96, 37, 173, 174,
+        176, 185, 246, 28, 70, 97, 105, 52, 64, 126, 15, 85, 71, 163, 35, 221,
+        81, 175, 58, 195, 92, 249, 206, 186, 197, 234, 38, 44, 83, 13, 110,
+        133, 40, 132, 9, 211, 223, 205, 244, 65, 129, 77, 82, 106, 220, 55,
+        200, 108, 193, 171, 250, 36, 225, 123, 8, 12, 189, 177, 74, 120, 136,
+        149, 139, 227, 99, 232, 109, 233, 203, 213, 254, 59, 0, 29, 57, 242,
+        239, 183, 14, 102, 88, 208, 228, 166, 119, 114, 248, 235, 117, 75, 10,
+        49, 68, 80, 180, 143, 237, 31, 26, 219, 153, 141, 51, 159, 17, 131,
+        20])
+    for i in range(len(M)/16):
+        for j in range(16):
+            c = M[i*16+j]
+            C[j] = chr(ord(C[j]) ^ ord(S[ord(c) ^ L]))
+            L = ord(C[j])
+    M += C
+    # Step 3. Initialize MD Buffer
+    X = list(chr(0) * 48)
+    # Step 4. Process Message in 16-Byte Blocks
+    for i in range(len(M)/16):
+        for j in range(16):
+            X[16+j] = M[i*16+j]
+            X[32+j] = chr(ord(X[16+j]) ^ ord(X[j]))
+        t = 0
+        for j in range(18):
+            for k in range(48):
+                X[k] = chr(ord(X[k]) ^ ord(S[t]))
+                t = ord(X[k])
+            t = (t+j) % 256
+    # Step 5. Output
+    return hexlify(''.join(X[0:16]))
